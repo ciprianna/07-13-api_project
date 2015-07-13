6d074ba2e5f5b2df17c32a9b4ec6e39492a03bd3
Ciprianna <ciprianna17@gmail.com>
2015-07-13 12:43:07 -0500

    Created empty controller files. Linked all models/controllers in   app.rb. Created layout file.

--- app.rb
+++ app.rb
@@ -11,6 +11,13 @@ require_relative "database_instance_methods.rb"
 
 # Models
 require_relative "models/assignments.rb"
+require_relative "models/collaborators.rb"
+require_relative "models/links.rb"
+require_relative "models/users.rb"
 
 # Controllers
 require_relative "controllers/main.rb"
+require_relative "controllers/assignments.rb"
+require_relative "controllers/collaborators.rb"
+require_relative "controllers/links.rb"
+require_relative "controllers/users.rb"
