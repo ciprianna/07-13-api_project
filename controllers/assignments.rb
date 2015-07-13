e996a3cd163de00c7f7b52c1fd42558776b49a96
Ciprianna <ciprianna17@gmail.com>
2015-07-13 14:02:41 -0500

    Created and tested views and routes for editing assignment info.

--- controllers/assignments.rb
+++ controllers/assignments.rb
@@ -24,3 +24,35 @@ get "/save_new_assignment" do
     erb :"assignments/add_assignment"
   end
 end
+
+# ------------------------------------------------------------------------------
+# Update assignment information
+# ------------------------------------------------------------------------------
+# Step 1 - Select assignment to Update
+get "/edit_assignment" do
+  erb :"assignments/edit_assignment"
+end
+
+# Step 2 - Display form for the user
+get "/edit_assignment/:id" do
+  @assignment_to_edit = Assignment.find(params["id"])
+  erb :"assignments/edit_assignment_form"
+end
+
+# Step 3 - Saved changed information
+get "/save_updated_assignment" do
+  @assignment_to_edit = Assignment.find(params["assignments"]["id"])
+  @assignment_to_edit.name = params["assignments"]["name"]
+  @assignment_to_edit.description = params["assignments"]["description"]
+  @assignment_to_edit.where_stored = params["assignments"]["where_stored"]
+
+  saved_assignment = @assignment_to_edit.save_valid
+
+  if (saved_assignment != false) && (!params["assignments"].nil?)
+    erb :"assignments/success"
+  else
+    @assignment_to_edit
+    @error = true
+    erb :"assignments/edit_assignment_form"
+  end
+end
