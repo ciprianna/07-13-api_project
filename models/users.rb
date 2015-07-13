118fd0447bcf2aede3796adad69bec8a44e470ac
Ciprianna <ciprianna17@gmail.com>
2015-07-13 12:38:42 -0500

    User model created.

---
+++ models/users.rb
@@ -0,0 +1,58 @@
+require_relative "../database_class_methods.rb"
+require_relative "../database_instance_methods.rb"
+
+class User
+  extend DatabaseClassMethods
+  include DatabaseInstanceMethods
+
+  attr_reader :id
+  attr_accessor :name
+
+  # Creates a new User Object
+  #
+  # options - empty Hash
+  #   id (optional) - Integer, Primary key
+  #   name (optional) - String, user's name
+  #
+  # Returns User Object
+  def initialize(options = {})
+    @id = options["id"]
+    @name = options["name"]
+  end
+
+  # Adds a new Object to the database if it has valid fields
+  #
+  # Returns the Object if it was added to the database or false if it failed
+  def add_to_database
+    if self.valid?
+      User.add({"name" => "#{self.name}"})
+    else
+      false
+    end
+  end
+
+  # Ensures that an updated Object has valid fields before saving
+  #
+  # Returns the Object if saved or false if save failed
+  def save_valid
+    if self.valid?
+      self.save
+    else
+      false
+    end
+  end
+
+  # Utility - Checks if the fields for a new Object are entered correctly
+  #
+  # Returns valid - true/false Boolean
+  def valid?
+
+    if self.name.nil? || self.name == ""
+      valid = false
+    end
+
+    return valid
+  end
+
+
+end
