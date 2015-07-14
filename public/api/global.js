d1e7d58b9b31a758eba374e14b722876be734bf9
Ciprianna <ciprianna17@gmail.com>
2015-07-13 22:30:16 -0500

    No progress. Still unable to add a new assignment without   refreshing the page. Moved js and css copies into sub folder   because it wasn't being read without it that way.

---
+++ public/api/global.js
@@ -0,0 +1,24 @@
+
+  var forms = document.forms;
+  var single_form = forms[0];
+  single_form[3].onclick = add_assignment;
+
+  function add_assignment(event){
+    var this_link = document.getElementById('/api/create_assignment');
+
+    var my_request = new XMLHttpRequest();
+
+    my_request.open("get", this_link);
+
+    my_request.addEventListener("load", function(){
+      var form_div = document.getElementById('add_new_assignment_form');
+      form_div.display = "none";
+      var success = document.getElementById("success");
+      var assignment_name = document.getElementById('assignment_name').value;
+      success.innerText = "You've successfully added " + assignment_name + " to the database."
+    })
+
+    event.preventDefault();
+
+    my_request.send();
+  }
--- public/global.js
+++ public/global.js
@@ -1,21 +1,24 @@
-var add_new_assignment_button = document.getElementById('use_api');
-add_new_assignment_button.addEventListener("click", add_assignment);
-
-function add_assignment(event){
-  var this_link = document.getElementById('/api/create_assignment');
-
-  var my_request = new XMLHttpRequest();
-
-  my_request.open("get", this_link);
-
-  my_request.addEventListener("load", function(){
-    var form_div = document.getElementById('add_new_assignment_form');
-    form_div.display = none;
-    var success = document.getElementById("success");
-    success.innerText = "You've successfully added " + params["assignment"]["name"] + " to the database."
-  })
-
-  event.preventDefault();
-
-  my_request.send();
-}
+// window.onload = function() {
+//   var add_new_assignment_button = document.getElementById('use_api');
+//   add_new_assignment_button.addEventListener("click", add_assignment);
+//
+//   function add_assignment(event){
+//     var this_link = document.getElementById('/api/create_assignment');
+//
+//     var my_request = new XMLHttpRequest();
+//
+//     my_request.open("get", this_link);
+//
+//     my_request.addEventListener("load", function(){
+//       var form_div = document.getElementById('add_new_assignment_form');
+//       form_div.display = "none";
+//       var success = document.getElementById("success");
+//       var assignment_name = document.getElementById('assignments[name]').value;
+//       success.innerText = "You've successfully added " + assignment_name + " to the database."
+//     })
+//
+//     event.preventDefault();
+//
+//     my_request.send();
+//   }
+// }
