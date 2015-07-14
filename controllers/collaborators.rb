408fed8943f4e3814f34710354b2b22685beb0f4
Ciprianna <ciprianna17@gmail.com>
2015-07-14 14:24:16 -0500

    Collaboration bridge.

--- controllers/collaborators.rb
+++ controllers/collaborators.rb
@@ -15,12 +15,12 @@ end
 
 # Step 2 - Save form information
 get "/save_new_collaborator" do
-  # @new_collaborator = Link.new({"name" => params['links']['name'], "assignment_id" => params['links']['assignment_id'], "description" => params["links"]["description"], "where_stored" => params["links"]["where_stored"]})
-  #
-  # if @new_link.add_to_database
-  #   erb :"links/success"
-  # else
-  #   @error = true
-  #   erb :"links/add_link"
-  # end
+  @new_collaborator = collaborator.new({"user_id" => params['collaborators']['user_id'], "assignment_id" => params['collaborators']['assignment_id']})
+
+  if @new_collaborator.add_to_database
+    erb :"collaborators/success"
+  else
+    @error = true
+    erb :"collaborators/add_collaborator"
+  end
 end
