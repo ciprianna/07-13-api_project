904840ba41d743651131379f107853ad0fb13abd
Ciprianna <ciprianna17@gmail.com>
2015-07-15 11:57:45 -0500

    Fixed bug in loading js correctly for all_assignments.

--- public/all_assignments.js
+++ public/all_assignments.js
@@ -1,17 +1,15 @@
+var all_assignments = document.getElementById('list');
+var route = new XMLHttpRequest();
+route.open("get", "/api/assignments");
 
-
-window.onload = function() {
-  var all_assignments = document.getElementById('list');
-  var route = new XMLHttpRequest();
-  route.open("get", "/api/assignments");
-  route.send();
-
-  // var parsed_var = route.response;
-  var parsed_all = JSON.parse(route.response);
+route.addEventListener("load", function(){
+  var parsed_all = JSON.parse(route.responseText);
 
   for (i = 0; i < parsed_all.length; i++) {
     var element = document.createElement("li");
     all_assignments.appendChild(element);
     element.appendChild(document.createTextNode(parsed_all[i].name));
   }
-}
+});
+
+route.send();
